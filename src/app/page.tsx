23c47aeafa2cb9897ccc57976a64d283236b0a3f
jeffreyroshan2006-crypto <jeffreyroshan2006@gmail.com>
2026-01-29 17:54:34 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Team from "@/components/Team";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
+
 export default function Home() {
   return (
         <div className="page-wrapper">
