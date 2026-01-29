607fe91b117f624287911b6633ecbb6ece852d2f
jeffreyroshan2006-crypto <jeffreyroshan2006@gmail.com>
2026-01-29 18:01:29 +0530

    Trigger fresh build after cache clear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import Team from "@/components/Team";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
-
 export default function Home() {
   return (
         <div className="page-wrapper">
